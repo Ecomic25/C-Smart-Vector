00b3cd603823c67bd55e857250242f6d50c2ed13
Ecomic25 <arthurcesarabcd@gmail.com>
2025-07-16 17:22:11 -0300

    Update intvec.h

--- intvec.h
+++ intvec.h
@@ -4,17 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// --- Definição da Estrutura ---
 typedef struct {
     int *data;
     int size;
     int capacity;
 } IntVector;
 
-// --- Macro de Atalho para Criação ---
 #define IV_CREATE(vec_name, cap) IntVector* vec_name = intvec_create(cap)
 
-// --- Protótipos das Funções (O "Cardápio") ---
 IntVector* intvec_create(int initial_capacity);
 void intvec_add(IntVector* v, int element);
 void intvec_destroy(IntVector* v);
